07d02bde62b1b3a16c564708353890210552004c
Bill Mountney <bill@mountney.com>
2020-05-01 14:52:06 -0400

    Added the ability to scale the banner size. Since the patches required to do this are more likely to break with a future game update, included a config file option to bypass them from being applied at all.

--- FriendlyTroopBannerHotkeys.cs
+++ FriendlyTroopBannerHotkeys.cs
@@ -1,4 +1,8 @@
 ﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
 using TaleWorlds.InputSystem;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.MountAndBlade.View.Missions;
@@ -9,7 +13,7 @@ namespace FriendlyTroopBannerHotkeys
     class FriendlyTroopBannerHotkeys
     {
         public const string ModName = "FriendlyTroopBannerHotkeys";
-        public const string ModVersion = "v1.1.0";
+        public const string ModVersion = "v1.2.0";
 
         // Start with option state from game settings
         static bool initialOptionState = ManagedOptions.GetConfig(ManagedOptions.ManagedOptionsType.ShowBannersOnFriendlyTroops) == 1f;
@@ -56,6 +60,96 @@ namespace FriendlyTroopBannerHotkeys
                 lastOptionState = newOptionState;
                 ManagedOptions.SetConfig(ManagedOptions.ManagedOptionsType.ShowBannersOnFriendlyTroops, newOptionState ? 1f : 0f);
             }
+        }
+    }
+
+    // Since these patches are more likely to break on a future game update, changed them to be applied via the Harmony manual patch method,
+    // so that they can be completely bypassed via a configuration file option.
+    //[HarmonyPatch]
+    public class MissionAgentLabelView_BannerSize_Patch
+    {
+        //[HarmonyPostfix]
+        //[HarmonyPatch(typeof(MissionAgentLabelView), "get__highlightedLabelScaleFactor")]
+        public static void Postfix_get__highlightedLabelScaleFactor(ref float __result)
+        {
+            // This is the scale factor for the yellow-outlined banner for selected troops, which works differently from scale for
+            // the unselected troop banners.  This scale factor is makes the banners for selected troops larger than normal scale
+            // when they are farther away, to make it easier to see that distant troops are selected. The default value is 30, and
+            // it seems that it can be lowered all the way down to 1, at which point this extra enlargement no longer happens.
+            // Values lower than 1 have no effect, so it can never be made smaller than the original banner size.
+            __result = FriendlyTroopBannerHotkeysModSettings.Settings.SelectedBannerScaleFactor;
+        }
+
+        //[HarmonyPostfix]
+        //[HarmonyPatch(typeof(MissionAgentLabelView), "get__labelBannerWidth")]
+        public static void Postfix_get__labelBannerWidth(ref float __result)
+        {
+            // This is the scale factor for the inner colored portion of the banner.
+            if (FriendlyTroopBannerHotkeysModSettings.Settings.ApplyBannerScalingMod)
+                __result *= FriendlyTroopBannerHotkeysModSettings.Settings.BannerScaleFactor;
+        }
+
+        //[HarmonyPostfix]
+        //[HarmonyPatch(typeof(MissionAgentLabelView), "get__labelBlackBorderWidth")]
+        public static void Postfix_get__labelBlackBorderWidth(ref float __result)
+        {
+            // This is the scale factor for the outer black border of the banner.
+            // I think it looks better keep this a little larger relative to the
+            // scaling of the inner portion as the scale gets smaller.
+            if (FriendlyTroopBannerHotkeysModSettings.Settings.ApplyBannerScalingMod)
+                __result *= FriendlyTroopBannerHotkeysModSettings.Settings.BannerScaleFactor +
+                    (1f - FriendlyTroopBannerHotkeysModSettings.Settings.BannerScaleFactor) / 100f;
+        }
+
+        //[HarmonyTranspiler]
+        //[HarmonyPatch(typeof(MissionAgentLabelView), "InitAgentLabel")]
+        public static IEnumerable<CodeInstruction> Transpiler_InitAgentLabel(IEnumerable<CodeInstruction> instructions)
+        {
+            // This adjusts the scale of the faction banner icon within the banner circle.
+            // Since this was controlled via constants in the code, it needed to be patched
+            // using the Harmony Transpiler method.
+            var codes = new List<CodeInstruction>(instructions);
+            if (FriendlyTroopBannerHotkeysModSettings.Settings.ApplyBannerScalingMod)
+            {
+                for (int i = 0; i < codes.Count; i++)
+                {
+                    if (codes[i].opcode == OpCodes.Callvirt)
+                    {
+                        String s = codes[i].operand.ToString();
+                        if (s == "Void SetVectorArgument(Single, Single, Single, Single)")
+                        {
+                            float? value;
+
+                            value = (codes[i - 4].operand as float?);
+                            if (value != null)
+                            {
+                                // This changes the horizontal scale, which seems to be a scale divider
+                                // instead of a multiplier.
+                                value *= (1.0f / FriendlyTroopBannerHotkeysModSettings.Settings.BannerScaleFactor);
+                                codes[i - 4].operand = value;
+                                // This changes the texture horizontal UV coordinate, which needs to be
+                                // adjusted to match the scale.
+                                codes[i - 2].operand = (1.0f - value) / 2.0f;
+                            }
+
+                            value = (codes[i - 3].operand as float?);
+                            if (value != null)
+                            {
+                                // This changes the vertical scale, which seems to be a scale divider
+                                // instead of a multiplier.
+                                value *= (1.0f / FriendlyTroopBannerHotkeysModSettings.Settings.BannerScaleFactor);
+                                codes[i - 3].operand = value;
+                                // This changes the texture vertical UV coordinate, which needs to be
+                                // adjusted to match the scale.
+                                codes[i - 1].operand = (1.0f - value) / 2.0f;
+                            }
+
+                            break;
+                        }
+                    }
+                }
+            }
+            return codes.AsEnumerable();
         }
     }
 }
--- FriendlyTroopBannerHotkeysSubModule.cs
+++ FriendlyTroopBannerHotkeysSubModule.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection;
 using HarmonyLib;
 using TaleWorlds.MountAndBlade;
+using TaleWorlds.MountAndBlade.View.Missions;
 
 namespace FriendlyTroopBannerHotkeys
 {
@@ -10,12 +13,41 @@ namespace FriendlyTroopBannerHotkeys
 		{
 			try
 			{
-				var harmony = new Harmony("com.mountney.bannerlord.togglefriendlytroopbanners");
-				harmony.PatchAll(typeof(FriendlyTroopBannerHotkeysSubModule).Assembly);
-			}
+				var harmony = new Harmony("com.mountney.bannerlord.friendlytroopbannerhotkeys");
+				harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+                // Since these patches are more likely to break on game version upgrades, allow them to be completely bypassed.
+                if (FriendlyTroopBannerHotkeysModSettings.Settings.ApplyBannerScalingMod)
+                {
+                    // For some reason the individual "GetRuntimeMethod" calls below were returning null, so I had to
+                    // use "GetRuntimeMethods" and then find the correct methods from that collection.
+                    var methods = typeof(MissionAgentLabelView).GetRuntimeMethods();
+
+                    //var original = typeof(MissionAgentLabelView).GetRuntimeMethod("get__highlightedLabelScaleFactor", new Type[] { });
+                    var original = methods.First(m => m.Name.Equals("get__highlightedLabelScaleFactor"));
+                    var patch = typeof(MissionAgentLabelView_BannerSize_Patch).GetMethod("Postfix_get__highlightedLabelScaleFactor");
+                    harmony.Patch(original, postfix: new HarmonyMethod(patch));
+
+                    //original = typeof(MissionAgentLabelView).GetRuntimeMethod("get__labelBannerWidth", new Type[] { });
+                    original = methods.First(m => m.Name.Equals("get__labelBannerWidth"));
+                    patch = typeof(MissionAgentLabelView_BannerSize_Patch).GetMethod("Postfix_get__labelBannerWidth");
+                    harmony.Patch(original, postfix: new HarmonyMethod(patch));
+
+                    //original = typeof(MissionAgentLabelView).GetRuntimeMethod("get__labelBlackBorderWidth", new Type[] { });
+                    original = methods.First(m => m.Name.Equals("get__labelBlackBorderWidth"));
+                    patch = typeof(MissionAgentLabelView_BannerSize_Patch).GetMethod("Postfix_get__labelBlackBorderWidth");
+                    harmony.Patch(original, postfix: new HarmonyMethod(patch));
+
+                    //original = typeof(MissionAgentLabelView).GetRuntimeMethod("InitAgentLabel", new Type[] { typeof(Agent), typeof(Banner) });
+                    original = methods.First(m => m.Name.Equals("InitAgentLabel"));
+                    patch = typeof(MissionAgentLabelView_BannerSize_Patch).GetMethod("Transpiler_InitAgentLabel");
+                    harmony.Patch(original, transpiler: new HarmonyMethod(patch));
+                }
+            }
 			catch (Exception ex)
 			{
-				Utility.Log("FriendlyTroopBannerHotkeys constructor", ex.Message);
+				Utility.Log("FriendlyTroopBannerHotkeys constructor", ex);
+                throw ex;
 			}
 		}
 
